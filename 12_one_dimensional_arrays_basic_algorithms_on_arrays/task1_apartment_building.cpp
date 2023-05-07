f94d40c322584e4d407849bcde41b02943f6de7e
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-07 17:56:27 +0300

    Update task1_apartment_building.cpp

--- 12_one_dimensional_arrays_basic_algorithms_on_arrays/task1_apartment_building.cpp
+++ 12_one_dimensional_arrays_basic_algorithms_on_arrays/task1_apartment_building.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
 
+int main ()
+{
+    std::string residents[10];
+    std::cout << "Input residents:" << std::endl;
+    for (int i = 0; i < 10; ++i)
+        std::cin >> residents[i];
+
+    std::cout << "Input number of apartament:" << std::endl;
+    for (int i = 0; i < 3; ++i) {
+        int num;
+        std::cin >> num;
+        if (num < 1 || num > 10)
+            std::cout << "You entered the wrong apartment number" << std::endl;
+        else
+            std::cout << residents[num - 1] << std::endl;
+    }
+    return 0;
+}
