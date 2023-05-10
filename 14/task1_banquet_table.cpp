34634d8a703c3762d2e4b2170589304d8ac8e670
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-10 19:26:00 +0300

    Create task1_banquet_table.cpp

---
+++ 14/task1_banquet_table.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main ()
+{
+    int cutlery[2][6] = {
+        {3, 3, 3, 3, 3, 4},
+        {3, 3, 3, 3, 3, 4}
+    };
+
+    int dishes[2][6] = {
+        {2, 2, 2, 2, 2, 3},
+        {2, 2, 2, 2, 2, 3}
+    };
+
+    int chairs[2][6] = {
+        {1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1}
+    };
+
+    chairs[0][4] += 1;
+    cutlery[1][2] -= 1;
+    cutlery[0][5] -= 1;
+    cutlery[1][2] += 1;
+    dishes[0][5] -= 1;
+
+    return 0;
+}
