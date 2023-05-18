d9a847c2862442b5bcfe47343fae4e1f9d6f49ce
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-18 08:57:31 +0300

    Create task2.cpp

---
+++ 15/task2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+void printTwoNum (std::vector<int>& v, int sum)
+{
+    int size = v.size();
+    for (int i = 0; i < size; ++i) {
+        for (int j = i + 1; j < size; ++j) {
+            if ((v[i] + v [j]) == sum) {
+                std::cout << std::endl << "Result: " << v[i] << " " << v[j] << std::endl;
+                return;
+            }
+        }
+    }
+    std::cout << "No pair of numbers" << std::endl;
+}
+
+int main ()
+{
+    std::cout << "Input size of array" << std::endl;
+    int size;
+    std::cin >> size;
+    std::vector<int>v(size);
+
+    std::cout << "Input " << size << " elements of array" << std::endl;
+    for (int i = 0; i < size; ++i)
+        std::cin >> v[i];
+
+    std::cout << std::endl << "You input:" << std::endl << '{';
+    for (int i = 0; i < size - 1; ++i) {
+        std::cout << v[i] << ", ";
+    }
+    std::cout << v[v.size() - 1] << '}' << std::endl;
+
+    std::cout << std::endl << "Input sum value:" << std::endl;
+    int sum;
+    std::cin >> sum;
+
+    printTwoNum(v, sum);
+    return 0;
+}
