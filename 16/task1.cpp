fee6305dd4c18b7e091151e735fc68303db7e68b
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-22 21:30:27 +0300

    Update task1.cpp

--- 16/task1.cpp
+++ 16/task1.cpp
@@ -2,18 +2,20 @@
 
 int main ()
 {
+    std::cout << "Input delta of speed: ";
+    double delta;
+    std::cin >> delta;
     double speed = 0.0;
     do {
-        std::cout << "Input delta of speed: ";
-        double delta;
-        std::cin >> delta;
         speed += delta;
         char speedStr[20];
         char units[] = " km/h";
         int n = std::sprintf (speedStr, "%.1f", speed);
         std::sprintf (&speedStr[n], "%s", units);
         std::cout << "Speed equal " << speedStr << std::endl;
-    } while (speed > 0.01 && speed < 150.01);
+        std::cout << "Input delta of speed: ";
+        std::cin >> delta;
+    } while (speed > 0.01 && speed < 150.01 && delta < 150);
 
     return 0;
 }
