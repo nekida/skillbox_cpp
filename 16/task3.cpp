c5e6b0b6062b704cb23c4ec5a4b8c9feb9ca6f2d
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-22 19:28:11 +0300

    Update task3.cpp

--- 16/task3.cpp
+++ 16/task3.cpp
@@ -2,27 +2,115 @@
 #include <string>
 #include <sstream>
 
+enum switches
+{
+    LIGHTS_INSIDE = 1,
+    LIGHTS_OUTSIDE = 2,
+    HEATERS = 4,
+    WATER_PIPE_HEATING = 8,
+    CONDITIONER = 16
+};
+
+static int switchesState = 0;
+
+bool isTurnGardenLighting (int time)
+{
+	return (time > 16 || time < 5);
+}
+
+bool isTempOfLight (int time)
+{
+	return (time > 16 && time <= 20);
+}
+
+void printState ()
+{
+    static int prevState = 0;
+    if ((switchesState ^ prevState) & LIGHTS_INSIDE) {
+        (switchesState & LIGHTS_INSIDE) ? std::cout << "Lights inside is ON" : std::cout << "Lights inside is OFF";
+        std::cout << std::endl;
+    }
+    if ((switchesState ^ prevState) & LIGHTS_OUTSIDE) {
+        (switchesState & LIGHTS_OUTSIDE) ? std::cout << "Lights outside is ON" : std::cout << "Lights outside is OFF";
+        std::cout << std::endl;
+    }
+    if ((switchesState ^ prevState) & HEATERS) {
+        (switchesState & HEATERS) ? std::cout << "Heaters is ON" : std::cout << "Heaters is OFF";
+        std::cout << std::endl;
+    }
+    if ((switchesState ^ prevState) & WATER_PIPE_HEATING) {
+        (switchesState & WATER_PIPE_HEATING) ? std::cout << "Water pipe heating is ON" : std::cout << "Water pipe heating is OFF";
+        std::cout << std::endl;
+    }
+    if ((switchesState ^ prevState) & CONDITIONER) {
+        (switchesState & CONDITIONER) ? std::cout << "Conditioner is ON" : std::cout << "Conditioner is OFF";
+        std::cout << std::endl;
+    }
+    prevState = switchesState;
+}
+
+void setState (switches state)
+{
+    switchesState |= state;
+}
+
+void resetState (switches state)
+{
+    switchesState &= ~state;
+}
+
 int main ()
 {
-	std::cout << "Input first value, operation and second value: " << std::endl;
-	std::string buffer;
-	std::cin >> buffer;
-	std::stringstream temp_stream(buffer);
+     int time = 0;
+     int tempOfLight = 5000;
+     int deltaTempOfLight = (5000 - 2700) / 4;
+     while (time <= 24) {
+        std::cout << "Time: " << time << ":00. Input states of sensors and light: " << std::endl;
+        std::string buffer, strMove, strLight;
+        getline(std::cin, buffer);
+        std::stringstream temp_stream(buffer);
 
-	char op;
-	double first, second;
-	temp_stream >> first >> op >> second;
+        double tempOutside, tempInside;
+        bool isMove, isTurnLighting;
 
-	if (op == '+')
-		std::cout << first + second;
-	else if (op == '-')
-		std::cout << first - second;
-	else if (op == '/') {
-		if (second == 0.0)
-			std::cout << "Div on zero" << std::endl;
-		std::cout << first / second;
-	} else if (op == '*')
-		std::cout << first * second;
+        temp_stream >> tempOutside >> tempInside >> strMove >> strLight;
 
-   return 0;
+        isMove = (strMove == "yes") ? true : false;
+        isTurnLighting = (strLight == "on") ? true : false;
+
+        if (tempOutside < 0.0)
+		    setState (WATER_PIPE_HEATING);
+	    else if (tempOutside > 5.0)
+		    resetState (WATER_PIPE_HEATING);
+
+        if (isTurnGardenLighting (time) && isMove)
+            setState (LIGHTS_OUTSIDE);
+        else
+            resetState (LIGHTS_OUTSIDE);
+
+        if (tempInside < 22.0)
+            setState (HEATERS);
+        else if (tempInside >= 25.0)
+            resetState (HEATERS);
+
+        if (tempInside >= 30.0)
+            setState (CONDITIONER);
+        else if (tempInside <= 25.0)
+            resetState (CONDITIONER);
+
+        if (isTempOfLight (time) && isTurnLighting) {
+            tempOfLight -= deltaTempOfLight;
+            std::cout << "color temperature is " << tempOfLight << std::endl;
+        }
+
+        printState ();
+
+        ++time;
+        if (time == 24) {
+            time = 0;
+            tempOfLight = 5000;
+        }
+     }
+
+    return 0;
 }
