003545f3ae7177cc45b928c1dabb25b7279e58ad
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-23 09:17:33 +0300

    Create task3.cpp

---
+++ 17/task3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+bool findStrInStr (char *str1, char *str2)
+{
+    if (strstr (str1, str2) == NULL)
+        return false;
+    return true;
+}
+
+int main ()
+{
+    std::cout << "Input 2 strings" << std::endl;
+    char s1[100], s2[100];
+    std::cin.getline (s1, 100);
+    std::cin.getline (s2, 100);
+    findStrInStr (s1, s2) ? std::cout << "true" : std::cout << "false";
+    std::cout << std::endl;
+
+    return 0;
+}
