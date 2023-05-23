707a218976413bde4f86489a4863efac8a1eb9a7
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-23 09:05:13 +0300

    Create task2.cpp

---
+++ 17/task2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void reverseArray (int *a)
+{
+    for (int i = 0, j = 9; i < 5; i++, j--) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
+
+int main ()
+{
+    std::cout << "Input 10 numbers" << std::endl;
+    int a[10];
+    for (int i = 0; i < 10; i++)
+        std::cin >> a[i];
+    reverseArray (a);
+    for (int i = 0; i < 10; i++) {
+        std::cout << a[i];
+        std::cout << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
