04ae97b25a4addb3eb83559f1197c315114ac8bf
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-24 08:22:42 +0300

    Create task2.cpp

---
+++ 18/task2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int getNumJumpVariants (int step)
+{
+    if (step < 0)
+        return 0;
+    else if (step == 0)
+        return 1;
+    else
+        return getNumJumpVariants (step - 3) + getNumJumpVariants (step - 2) + getNumJumpVariants (step - 1);
+}
+
+int main ()
+{
+    std::cout << "Input num  step" << std::endl;
+    int n;
+    std::cin >> n;
+    std::cout << getNumJumpVariants (n) << std::endl;
+    return 0;
+}
