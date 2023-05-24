e50ee5d37e2f8b9c47dd3f8b0fc77c3f543b3a5c
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-24 08:54:37 +0300

    Create task4.cpp

---
+++ 18/task4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void evendigits (long long n, int& ans)
+{
+    if (n % 10 % 2 == 0)
+        ans++;
+    if (n > 10)
+        evendigits (n / 10, ans);
+}
+
+int main ()
+{
+    std::cout << "Input long long num" << std::endl;
+    long long n;
+    int k;
+    std::cin >> n;
+    evendigits (n, k);
+    std::cout << k << std::endl;
+    return 0;
+}
