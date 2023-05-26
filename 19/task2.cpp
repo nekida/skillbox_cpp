349c740ece6d272cea15eaf7922bea2b09b5ad53
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-05-26 08:24:45 +0300

    Create task2.cpp

---
+++ 19/task2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main ()
+{
+    std::cout << "Enter file path" << std::endl;
+    std::string path;
+    std::cin >> path;
+
+    std::ifstream file;
+    file.open(path, std::ios::binary);
+    if (!file.is_open()) {
+        std::cout << "Error open file" << std::endl;
+        return 0;
+    }
+    char buf[100];
+    while (!file.eof()) {
+        file.read(buf, sizeof (buf));
+        auto size = file.gcount();
+        if ((long unsigned int)size < sizeof (buf))
+            buf[size] = '\0';
+        std::cout << buf;
+    }
+    file.close();
+
+    return 0;
+}
