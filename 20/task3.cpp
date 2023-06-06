b43942081e3ee5350b63a48a483ec709628a75f4
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-06-06 09:57:40 +0300

    Update task3.cpp

--- 20/task3.cpp
+++ 20/task3.cpp
@@ -4,39 +4,41 @@
 
 int main ()
 {
-    std::ifstream fileIn ("river.txt");
-    if (!fileIn.is_open()) {
-        std::cout << "Error open file river.txt" << std::endl;
-        return 0;
-    }
-
-    std::cout << "Enter the type of fish you want to catch" << std::endl;
-    std::string catchFish;
-    std::cin >> catchFish;
+    bool isFind = true;
     int fishCounter = 0;
-
-    while (!fileIn.eof()) {
-        std::string riverFish;
-        fileIn >> riverFish;
-        if (catchFish == riverFish) {
-            std::ofstream fileOut ("basket.txt", std::ofstream::app);
-            if (!fileOut.is_open()) {
-                std::cout << "Error open file basket.txt" << std::endl;
-                return 0;
-            }
-            fileOut << riverFish << std::endl;
-            fishCounter++;
-            fileOut.close ();
-        }
-        else
-            continue;
-
+    while (isFind) {
         std::cout << "Enter the type of fish you want to catch" << std::endl;
+        std::string catchFish;
         std::cin >> catchFish;
-    }
 
+        std::ifstream fileIn ("river.txt");
+        if (!fileIn.is_open()) {
+            std::cout << "Error open file river.txt" << std::endl;
+            return 0;
+        }
+
+        int tmp = fishCounter;
+        while (!fileIn.eof()) {
+            std::string riverFish;
+            fileIn >> riverFish;
+            if (catchFish == riverFish) {
+                std::ofstream fileOut ("basket.txt", std::ofstream::app);
+                if (!fileOut.is_open()) {
+                    std::cout << "Error open file basket.txt" << std::endl;
+                    return 0;
+                }
+                fileOut << riverFish << std::endl;
+                fishCounter++;
+                fileOut.close ();
+            }
+            else
+                continue;
+
+        }
+        isFind = tmp != fishCounter;
+        fileIn.close();
+    }
     std::cout << "Fish were caught for the current catch " << fishCounter << std::endl;
-    fileIn.close();
 
     return 0;
 }
