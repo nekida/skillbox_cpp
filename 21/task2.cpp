1db929d62397f6a67e030ce95ebf18baa0e10030
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-07-11 17:05:50 +0300

    Update task2.cpp

--- 21/task2.cpp
+++ 21/task2.cpp
@@ -3,8 +3,7 @@
 #include <cmath>
 #include <vector>
 
-enum RoomType
-{
+enum RoomType {
     bedroom,
     kitchen,
     bathroom,
@@ -12,8 +11,7 @@ enum RoomType
     living
 };
 
-enum BuildType
-{
+enum BuildType {
     garage,
     shed,
     sauna
@@ -35,11 +33,11 @@ struct House {
 };
 
 struct OtherBuilding {
-    RoomType type;
+    BuildType type;
     float s;
 };
 
-struct region {
+struct Region {
     unsigned ID;
     std::vector<House> houses;
     std::vector<OtherBuilding> otherBuilding;
@@ -47,41 +45,72 @@ struct region {
 
 int main ()
 {
+    std::vector<Region> village;
+
     std::cout << "Enter the number of regions:" << std::endl;
     int numRegions;
     std::cin >> numRegions;
 
-    std::vector<region> village(numRegions);
     for (int i = 0; i < numRegions; ++i) {
+        Region region;
         std::cout << "Enter the ID of region " << i + 1 << ":" << std::endl;
-        std::cin >> village[i].ID;
+        std::cin >> region.ID;
+
         std::cout << "Enter the number of houses on region " << i + 1 << ":" << std::endl;
         int numHouses;
         std::cin >> numHouses;
-        village[i].houses.resize (numHouses);
+
         for (int j = 0; j < numHouses; ++j) {
-            std::cout << "Enter the number of levels on house " << j + 1 << " (1-3):" << std::endl;
+            House house;
+            std::cout << "Enter the area of house " << j + 1 << " (1..3):" << std::endl;
+            std::cin >> house.s;
+            std::cout << "Enter the number of levels on house " << j + 1 << " (1..3):" << std::endl;
             int numLevels;
             std::cin >> numLevels;
-            village[i].houses[j].levels.resize (numLevels);
+
             for (int k = 0; k < numLevels; ++k) {
-                std::cout << "Enter the number of rooms on level " << k + 1 << ":" << std::endl;
+                Level level;
+                std::cout << "Enter the ceiling height of level " << k + 1 << " (1..3):" << std::endl;
+                std::cin >> level.h;
+                std::cout << "Enter the number of rooms on level " << k + 1 << " (2..4):" << std::endl;
                 int numRooms;
                 std::cin >> numRooms;
-                village[i].houses[j].levels[k].rooms.resize (numRooms);
-                for (int l = 0; l < numLevels; ++l) {
+
+                for (int l = 0; l < numRooms; ++l) {
+                    Room room;
                     int numRoom = l + 1;
                     std::cout << "Enter the type of room " << numRoom << ":" << std::endl;
                     std::cout << "0 - bedroom, 1 - kitchen, 2 - bathroom, 3 - children, 4 - living" << std::endl;
                     int type;
                     std::cin >> type;
-                    village[i].houses[j].levels[k].rooms[l].type = (RoomType)type;
-
+                    room.type = (RoomType)type;
                     std::cout << "Enter the area of room " << numRoom << ":" << std::endl;
-                    std::cin >> village[i].houses[j].levels[k].rooms[l].s;
+                    std::cin >> room.s;
+
+                    level.rooms.push_back(room);
                 }
+                house.levels.push_back(level);
             }
+            region.houses.push_back(house);
         }
+
+        std::cout << "Enter the number of other buildings on region " << i + 1 << ":" << std::endl;
+        int numOtherBuild;
+        std::cin >> numOtherBuild;
+
+        for (int j = 0; j < numOtherBuild; ++j) {
+            OtherBuilding otherBuilding;
+            std::cout << "Enter the area of other building " << j + 1 << std::endl;
+            std::cin >> otherBuilding.s;
+            std::cout << "Enter the type of other building " << j + 1 << " (1..3):" << std::endl;
+            std::cout << "0 - garage, 1 - shed, 2 - sauna" << std::endl;
+            int type;
+            std::cin >> type;
+            otherBuilding.type = (BuildType)type;
+            region.otherBuilding.push_back(otherBuilding);
+        }
+
+        village.push_back(region);
     }
 
     return 0;
