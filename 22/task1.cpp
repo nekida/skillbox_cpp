2be0d08998f997410da2bb631ac728fe7339ece3
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-07-19 08:48:08 +0300

    Update task1.cpp

--- 22/task1.cpp
+++ 22/task1.cpp
@@ -33,11 +33,32 @@ void getData (DataWithStrings& strings)
 
 int main ()
 {
-    std::map<std::string, std::string> telBook;
+    std::map<std::string, std::string> NumName;
+    std::map<std::string, std::string> NameNum;
     DataWithStrings data;
     getline(std::cin, data.input, '\n');
     getData(data);
-    std::cout << data.outputName << " : " << data.outputNum << std::endl;
+
+    while (data.outputName != "" || data.outputNum != "") {
+        if (data.outputName == "")
+            std::cout << NumName[data.outputNum] << std::endl;
+        else if (data.outputNum == "") {
+            for (auto it = NumName.find(NameNum[data.outputName]); it->second == data.outputName; --it) {
+                if (it == NumName.begin()) {
+                    std::cout << it->first << std::endl;
+                    break;
+                }
+                std::cout << it->first << ' ';
+            }
+        }
+        else {
+            NumName[data.outputNum] = data.outputName;
+            NameNum[data.outputName] = data.outputNum;
+        }
+
+        getline(std::cin, data.input, '\n');
+        getData(data);
+    }
 
     return 0;
 }
