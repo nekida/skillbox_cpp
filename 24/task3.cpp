fc060ea8efa9bc52c015da6b43708eec4f4c3498
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-07-25 19:34:55 +0300

    Update task3.cpp

--- 24/task3.cpp
+++ 24/task3.cpp
@@ -6,10 +6,30 @@
 
 int main ()
 {
-    std::cout << "Enter minutes and seconds (mm-ss): " << std::endl;
+    std::cout << "Enter minutes and seconds (mm:ss):" << std::endl;
     int min, sec;
     char temp;
     std::cin >> min >> temp >> sec;
+
+    std::time_t curTime = std::time(nullptr);
+    std::tm targetTime = *std::localtime(&curTime);
+    std::tm *tickTime = std::localtime(&curTime);
+    targetTime.tm_min += min;
+    targetTime.tm_sec += sec;
+    int seconds = min * 60 + sec;
+
+    while (targetTime.tm_min - tickTime->tm_min || targetTime.tm_sec - tickTime->tm_sec) {
+        int saveSec = tickTime->tm_sec;
+        std::time_t newTime = std::time(nullptr);
+        tickTime = std::localtime(&newTime);
+        if (saveSec != tickTime->tm_sec) {
+            int tempMin = seconds > 60 ? (seconds / 60) : 0;
+            int tempSec = seconds > 60 ? (seconds % 60) : seconds;
+            std::cout << "Time left -> " << tempMin << ':' << tempSec << std::endl;
+            seconds--;
+        }
+    }
+    std::cout << "DING! DING! DING!" << std::endl;
 
     return 0;
 }
