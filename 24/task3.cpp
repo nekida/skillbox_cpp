3e6216f417c25173a94336a2beb45baea5a5e394
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-07-25 09:02:32 +0300

    Create task3.cpp

---
+++ 24/task3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <iomanip>
+
+int main ()
+{
+    std::cout << "Enter minutes and seconds (mm-ss): " << std::endl;
+    int min, sec;
+    char temp;
+    std::cin >> min >> temp >> sec;
+
+    return 0;
+}
