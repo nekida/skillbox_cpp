34080f608eeb883370ae451eead553348a01596f
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-07-25 08:08:32 +0300

    Update task2.cpp

--- 24/task2.cpp
+++ 24/task2.cpp
@@ -4,41 +4,61 @@
 #include <vector>
 #include <iomanip>
 
-struct
+struct Date {
+    int year;
+    int month;
+    int day;
+};
 
 struct MyFriend {
     std::string name;
-    std::tm bithday;
+    Date birthday;
 };
+
+const int year[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+bool isLeap (int checkYear)
+{
+    return (checkYear % 400 == 0) || ((checkYear % 4 == 0) && (checkYear % 100 != 0));
+}
+
+int getNumOfDaysInYear (const Date& date, int curYear)
+{
+    int numOfDays = 0;
+    for (int i = 0; i < date.month - 1; i++) {
+        numOfDays += year[i];
+        if (i == 1 && isLeap (curYear))
+            numOfDays += 1;
+    }
+    return numOfDays + date.day - 1;
+}
 
 void findTheNearestBirthday (std::vector<MyFriend>& myFriends)
 {
     int delta = 365;
-    int prevDelta = 365;
     std::vector<MyFriend> findFriends;
 
     std::time_t t = std::time(nullptr);
     std::tm* local = std::localtime(&t);
     for (auto& myFriend : myFriends) {
-        if (myFriend.bithday.tm_mon == local->tm_mon && myFriend.bithday.tm_mday == local->tm_mday) {
-            std::cout << "Today bithday is " << myFriend.name << std::endl;
+        if (myFriend.birthday.month - 1 == local->tm_mon && myFriend.birthday.day == local->tm_mday) {
+            std::cout << "Today birthday is " << myFriend.name << std::endl;
         } else {
-            std::cout << myFriend.bithday.tm_yday;
-            std::cout << local->tm_yday;
-            int tempDelta = myFriend.bithday.tm_yday - local->tm_yday;
-            if (tempDelta < delta)
+            int tempDelta = getNumOfDaysInYear(myFriend.birthday, local->tm_year) - local->tm_yday;
+            if (tempDelta < delta && tempDelta > 0)
                 delta = tempDelta;
         }
     }
 
     for (auto& myFriend : myFriends) {
-        if (myFriend.bithday.tm_yday == local->tm_yday + delta)
+        if (getNumOfDaysInYear(myFriend.birthday, local->tm_year) == local->tm_yday + delta)
             findFriends.push_back(myFriend);
     }
 
     if (!findFriends.empty())
         for (auto& myFriend : findFriends)
-            std::cout << "Nearest bithday of " << myFriend.name  << " - " << std::put_time(&myFriend.bithday, "%m/%d") << std::endl;
+            std::cout << "Nearest birthday of " << myFriend.name  << " - " << myFriend.birthday.month
+                                                    << '/' << myFriend.birthday.day << std::endl;
 }
 
 int main ()
@@ -48,16 +68,12 @@ int main ()
     std::cout << "Input name of friend or command \"end\"" << std::endl;
     std::cin >> input;
     while (input != "end") {
-        MyFriend myFriend;   
+        MyFriend myFriend;
         myFriend.name = input;
-
-        std::time_t t = std::time(nullptr);
-        myFriend.bithday = *std::localtime(&t);
-        std::cout << "Input bithday date of friend (yyyy/mm/dd)" << std::endl;
-        std::cin >> std::get_time(&myFriend.bithday, "%Y/%m/%d");
+        std::cout << "Input birthday date of friend (yyyy/mm/dd)" << std::endl;
+        char temp;
+        std::cin >> myFriend.birthday.year >> temp >> myFriend.birthday.month >> temp >> myFriend.birthday.day;
         myFriends.push_back(myFriend);
-
-        // std::cout << myFriend.name << " - " << std::put_time(&myFriend.bithday, "%m/%d") << std::endl;
 
         std::cout << "Input name of friend or command \"end\"" << std::endl;
         std::cin >> input;
