afa1b8771fff05ab8f544ef2a786308e5fd5187c
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-07-26 19:01:07 +0300

    Update task3.cpp

--- 24/task3.cpp
+++ 24/task3.cpp
@@ -7,24 +7,23 @@
 int main ()
 {
     std::cout << "Enter minutes and seconds (mm:ss):" << std::endl;
-    int min, sec;
-    char temp;
-    std::cin >> min >> temp >> sec;
+    std::tm time;
+    std::cin >> std::get_time(&time, "%M:%S");
 
     std::time_t curTime = std::time(nullptr);
     std::tm targetTime = *std::localtime(&curTime);
     std::tm *tickTime = std::localtime(&curTime);
-    targetTime.tm_min += min;
-    targetTime.tm_sec += sec;
-    int seconds = min * 60 + sec;
+    targetTime.tm_min += time.tm_min;
+    targetTime.tm_sec += time.tm_sec;
+    int seconds = time.tm_min * 60 + time.tm_sec;
 
     while (targetTime.tm_min - tickTime->tm_min || targetTime.tm_sec - tickTime->tm_sec) {
         int saveSec = tickTime->tm_sec;
         std::time_t newTime = std::time(nullptr);
         tickTime = std::localtime(&newTime);
         if (saveSec != tickTime->tm_sec) {
-            int tempMin = seconds > 60 ? (seconds / 60) : 0;
-            int tempSec = seconds > 60 ? (seconds % 60) : seconds;
+            int tempMin = seconds > 59 ? (seconds / 59) : 0;
+            int tempSec = seconds >= 60 ? (seconds % 60) : seconds;
             std::cout << "Time left -> " << tempMin << ':' << tempSec << std::endl;
             seconds--;
         }
