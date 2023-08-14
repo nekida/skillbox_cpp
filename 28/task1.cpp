44c3187abc5d4fc3ca82e505ddfbe10e44813793
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-08-14 18:35:51 +0300

    Update task1.cpp

--- 28/task1.cpp
+++ 28/task1.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <vector>
 #include <mutex>
+#include <algorithm>
 
 class Swimmer {
     std::string name;
@@ -24,16 +25,23 @@ public:
     bool isFinished = false;
 
     size_t time = 0;
+
+    std::thread thr;
 };
 
 std::mutex stdio_access;
 
-void distanceUpdate (std::string name, size_t distance)
+void distanceUpdate (Swimmer& swimmer)
 {
     std::this_thread::sleep_for(std::chrono::seconds(1));
     stdio_access.lock();
-    std::cout << name;
-    distance < 100 ? std::cout << " swimming " << distance << " meters" : std::cout << " finished";
+    std::cout << swimmer.getName();
+    if (!swimmer.isFinished) {
+        swimmer.getDistance() += swimmer.getSpeed();
+        swimmer.time++;
+        swimmer.isFinished = swimmer.getDistance() >= 100;
+    }
+    swimmer.isFinished ? std::cout << " finished" : std::cout << " swimming " << swimmer.getDistance() << " meters";
     std::cout << std::endl;
     stdio_access.unlock();
 }
@@ -41,7 +49,7 @@ void distanceUpdate (std::string name, size_t distance)
 int main ()
 {
     constexpr int numSwimmers = 6;
-    Swimmer swimmers[numSwimmers];
+    std::vector<Swimmer> swimmers(numSwimmers);
     for (size_t i = 0; i < numSwimmers; ++i) {
         std::cout << "Enter the name and speed of swimmer number " << i + 1 << std::endl;
         std::cin >> swimmers[i].getName();
@@ -53,45 +61,22 @@ int main ()
     bool isAll = false;
     while(!isAll) {
         isAll = true;
-        std::thread swim0Thread(distanceUpdate, swimmers[0].getName(), swimmers[0].getDistance());
-        std::thread swim1Thread(distanceUpdate, swimmers[1].getName(), swimmers[1].getDistance());
-        std::thread swim2Thread(distanceUpdate, swimmers[2].getName(), swimmers[2].getDistance());
-        std::thread swim3Thread(distanceUpdate, swimmers[3].getName(), swimmers[3].getDistance());
-        std::thread swim4Thread(distanceUpdate, swimmers[4].getName(), swimmers[4].getDistance());
-        std::thread swim5Thread(distanceUpdate, swimmers[5].getName(), swimmers[5].getDistance());
-        swim0Thread.join();
-        swim1Thread.join();
-        swim2Thread.join();
-        swim3Thread.join();
-        swim4Thread.join();
-        swim5Thread.join();
+
+        for (size_t i = 0; i < numSwimmers; ++i)
+            swimmers[i].thr = std::thread(distanceUpdate, std::ref(swimmers[i]));
+
+        for (size_t i = 0; i < numSwimmers; ++i)
+            swimmers[i].thr.join();
 
         for (size_t i = 0; i < numSwimmers; ++i) {
-            if (!swimmers[i].isFinished) {
-                swimmers[i].time++;
-                swimmers[i].getDistance() += swimmers[i].getSpeed();
-                swimmers[i].isFinished = swimmers[i].getDistance() >= 100;
-            }
             isAll &= swimmers[i].isFinished;
         }
-
-        swim0Thread.~thread();
-        swim1Thread.~thread();
-        swim2Thread.~thread();
-        swim3Thread.~thread();
-        swim4Thread.~thread();
-        swim5Thread.~thread();
     }
 
-    for (size_t i = 0; i < numSwimmers; ++i) {
-        for (size_t j = i + 1; j < numSwimmers; ++j) {
-            if (swimmers[i].time > swimmers[j].time) {
-                auto swimmer = swimmers[i];
-                swimmers[i] = swimmers[j];
-                swimmers[j] = swimmer;
-            }
-        }
-    }
+    std::sort(swimmers.begin(), swimmers.end(),
+            [](const Swimmer& a, const Swimmer& b) {
+                return a.time < b.time;
+            });
 
     size_t num = 1;
     for (auto& swimmer : swimmers) {
