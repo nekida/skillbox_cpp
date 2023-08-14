5fe7caa8cd12aeb3c2d5dda03e69a6c613dd1f6e
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-08-14 15:31:14 +0300

    Update task1.cpp

--- 28/task1.cpp
+++ 28/task1.cpp
@@ -3,10 +3,13 @@
 #include <thread>
 #include <ctime>
 #include <vector>
+#include <mutex>
 
 class Swimmer {
     std::string name;
+
     size_t speedMPS = 0;
+
     size_t distance = 0;
 
 public:
@@ -17,11 +20,22 @@ public:
     void setSpeed(size_t speed) { speedMPS = speed; }
 
     size_t& getDistance() { return distance; }
+
+    bool isFinished = false;
+
+    size_t time = 0;
 };
 
-void distanceUpdate ()
+std::mutex stdio_access;
+
+void distanceUpdate (std::string name, size_t distance)
 {
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    stdio_access.lock();
+    std::cout << name;
+    distance < 100 ? std::cout << " swimming " << distance << " meters" : std::cout << " finished";
+    std::cout << std::endl;
+    stdio_access.unlock();
 }
 
 int main ()
@@ -36,20 +50,54 @@ int main ()
         swimmers[i].setSpeed(speed);
     }
 
-    std::thread swimmersThreads[numSwimmers];
-    for (size_t i = 0; i < numSwimmers; ++i)
-        swimmersThreads[0] = std::thread(distanceUpdate);
-
     bool isAll = false;
-    while (!isAll) {
+    while(!isAll) {
         isAll = true;
+        std::thread swim0Thread(distanceUpdate, swimmers[0].getName(), swimmers[0].getDistance());
+        std::thread swim1Thread(distanceUpdate, swimmers[1].getName(), swimmers[1].getDistance());
+        std::thread swim2Thread(distanceUpdate, swimmers[2].getName(), swimmers[2].getDistance());
+        std::thread swim3Thread(distanceUpdate, swimmers[3].getName(), swimmers[3].getDistance());
+        std::thread swim4Thread(distanceUpdate, swimmers[4].getName(), swimmers[4].getDistance());
+        std::thread swim5Thread(distanceUpdate, swimmers[5].getName(), swimmers[5].getDistance());
+        swim0Thread.join();
+        swim1Thread.join();
+        swim2Thread.join();
+        swim3Thread.join();
+        swim4Thread.join();
+        swim5Thread.join();
+
         for (size_t i = 0; i < numSwimmers; ++i) {
-            swimmersThreads[i].join();
-            swimmers->getDistance() += swimmers->getSpeed();
-            isAll &= swimmers[i].getDistance() >= 100;
+            if (!swimmers[i].isFinished) {
+                swimmers[i].time++;
+                swimmers[i].getDistance() += swimmers[i].getSpeed();
+                swimmers[i].isFinished = swimmers[i].getDistance() >= 100;
+            }
+            isAll &= swimmers[i].isFinished;
+        }
+
+        swim0Thread.~thread();
+        swim1Thread.~thread();
+        swim2Thread.~thread();
+        swim3Thread.~thread();
+        swim4Thread.~thread();
+        swim5Thread.~thread();
+    }
+
+    for (size_t i = 0; i < numSwimmers; ++i) {
+        for (size_t j = i + 1; j < numSwimmers; ++j) {
+            if (swimmers[i].time > swimmers[j].time) {
+                auto swimmer = swimmers[i];
+                swimmers[i] = swimmers[j];
+                swimmers[j] = swimmer;
+            }
         }
     }
 
+    size_t num = 1;
+    for (auto& swimmer : swimmers) {
+        std::cout << num << " " << swimmer.getName() << " - " << swimmer.time << std::endl;
+        num++;
+    }
 
     return 0;
 }
