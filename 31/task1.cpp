949d1c93dcf7929ab5eb4c685678a8256bed659e
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2023-09-05 08:47:50 +0300

    Update task1.cpp
    start

--- 31/task1.cpp
+++ 31/task1.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
 
+class Toy {
+    std::string name;
+
+public:
+    Toy(const std::string& name_) : name(name_) {}
+
+    std::string getName() const { return name; }
+
+    ~Toy() {
+        std::cout << "Toy " << name << " was dropped" << std::endl;
+    }
+};
+
+class Dog {
+    std::shared_ptr<Toy> toySharedPtr;
+
+public:
+    void getToy(std::shared_ptr<Toy> getToySharedPtr) {
+        if (getToySharedPtr == nullptr)
+            return;
+        if (getToySharedPtr.get()->getName() == toySharedPtr.get()->getName()) {
+            std::cout << "I already have this toy" << std::endl;
+            return;
+        }
+        if (getToySharedPtr.use_count() > 1) {
+            std::cout << "Another dog is playing with this toy" << std::endl;
+            return;
+        }
+        toySharedPtr = getToySharedPtr;
+    }
+
+    void dropToy() {
+        if (toySharedPtr == nullptr)
+            std::cout << "Nothing to drop" << std::endl;
+        else
+            toySharedPtr.reset();
+    }
+};
+
+int main ()
+{
+    Dog dog1, dog2;
+    Toy toy("noName");
+    std::cout << "Hello" << std::endl;
+    return 0;
+}
